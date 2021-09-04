02d4c9a755a0eda235fdfe90218e1ba458c4853f
demolitionok <demolitionok@gmail.com>
2021-09-04 15:48:01 +0300

    feat: implement IDamageReceiver for Soldier

--- Assets/Scripts/Unit/Soldier.cs
+++ Assets/Scripts/Unit/Soldier.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Soldier : AbstractUnit
+public class Soldier : AbstractUnit, IDamageReceiver
 {
-    
+    public void ReceiveDamage(Damage damage)
+    {
+        SetHp(GetHp() - damage.DmgValue);
+    }
 }
