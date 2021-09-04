1d13e2ef08862256fb1fd11480b789b3a41c9a73
demolitionok <demolitionok@gmail.com>
2021-09-04 14:40:04 +0300

    feat: add AbstractUnit class
    Название AbstractUnit а не Unit что бы не перекрывать стандартный
    юнитишный класс

---
+++ Assets/Scripts/Unit/AbstractUnit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractUnit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
