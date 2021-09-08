9c8ade2efd0b047351721184335a0c1b505c68d0
demolitionok <demolitionok@gmail.com>
2021-09-08 12:44:38 +0300

    feat: add OnHpChanged event

--- Assets/Scripts/Unit/AbstractUnit.cs
+++ Assets/Scripts/Unit/AbstractUnit.cs
@@ -8,17 +8,34 @@ public abstract class AbstractUnit : MonoBehaviour
     [SerializeField]
     private float _hp;
     public event Action OnDeath;
-    
+    public event Action<float> OnHpChanged;
+
     protected void SetHp(float value)
     {
         _hp = value;
-        if (value <= 0)
+        OnHpChanged?.Invoke(value);
+    }
+
+    private void CheckForDeath(float hp)
+    {
+        if (hp <= 0)
         {
+            _hp = 0;
             OnDeath?.Invoke();
             Destroy(gameObject);
-            _hp = 0;
         }
     }
 
     public float GetHp() => _hp;
+
+    private void OnEnable()
+    {
+        OnHpChanged += CheckForDeath;
+    }
+
+    private void OnDestroy()
+    {
+        OnHpChanged -= CheckForDeath;
+    }
+
 }
