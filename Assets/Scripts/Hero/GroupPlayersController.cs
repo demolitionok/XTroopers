e308795459e0abd1d0340866da4f7f598a28e3bc
wolf4ok <demon.roge@gmail.com>
2021-10-03 21:43:59 +0300

    fix group controll

--- Assets/Scripts/Hero/GroupPlayersController.cs
+++ Assets/Scripts/Hero/GroupPlayersController.cs
@@ -1,51 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+struct UnitProp
+{
+    public Animator unitAnim;
+    public Transform unitTransform;
+    public NavMeshAgent unitNavMesh;
+}
+
 public class GroupPlayersController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private int _maxGroup;
     [SerializeField] private FixedJoystick _joystick;
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private Animator _anim;
-    [SerializeField] private PhysicsMovement _mov;
-    [SerializeField] private Rigidbody _rigidbody1;
-    [SerializeField] private Animator _anim1;
-    [SerializeField] private Transform _transform1;
-    [SerializeField] private PhysicsMovement _mov1;
-    [SerializeField] private Rigidbody _rigidbody2;
-    [SerializeField] private Animator _anim2;
-    [SerializeField] private Transform _transform2;
-    [SerializeField] private PhysicsMovement _mov2;
+    
+    [Header("Hero")]
+    public List<GameObject> Heroes;
+    
+    private List<UnitProp> heroProp;
+    private List<Vector3> heroPosPattern;
+    private NavMeshAgent leaderNavMesh;
 
     void Start()
     {
-        
+        leaderNavMesh = GetComponent<NavMeshAgent>();
+        GeneratePosPattern(); //запускать при смене количества персов
+        GetHeroProp(); //запускать при смене количества персов
     }
+
     void Update()
     {
-        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, 0, _joystick.Vertical * _moveSpeed);
-        _rigidbody1.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, 0, _joystick.Vertical * _moveSpeed);
-        _rigidbody2.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, 0, _joystick.Vertical * _moveSpeed);
-        _mov.Move(new Vector3(_joystick.Vertical * _moveSpeed, 0, _joystick.Horizontal * _moveSpeed));
-        _mov1.Move(new Vector3(_joystick.Vertical * _moveSpeed, 0, _joystick.Horizontal * _moveSpeed));
-        _mov2.Move(new Vector3(_joystick.Vertical * _moveSpeed, 0, _joystick.Horizontal * _moveSpeed));
+       GroupHeroMove();
+    }
+
+    private void GroupHeroMove(){
         if (_joystick.Horizontal !=0 || _joystick.Vertical !=0)
         {
-            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
-            _transform1.rotation = Quaternion.LookRotation(_rigidbody1.velocity);
-            _transform2.rotation = Quaternion.LookRotation(_rigidbody2.velocity);
-            _anim.SetBool("isRun", true);
-            _anim1.SetBool("isRun", true);
-            _anim2.SetBool("isRun", true);
+            var moveDirection = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+            var movePosition = transform.position + moveDirection;
+            leaderNavMesh.SetDestination(movePosition);
+
+            for(var i = 0; i < Heroes.Count; i++)
+            {
+                var heroMovePosition = transform.position + heroPosPattern[i];
+                heroProp[i].unitNavMesh.SetDestination(heroMovePosition);
+                heroProp[i].unitAnim.SetBool("isRun", true);
+            }
         }
         else
         {
-            _anim.SetBool("isRun", false);
-            _anim1.SetBool("isRun", false);
-            _anim2.SetBool("isRun", false);
+            for(var i = 0; i < Heroes.Count; i++)
+            {
+                float X = heroProp[i].unitTransform.position.x - transform.position.x;
+                float Y = heroProp[i].unitTransform.position.y - transform.position.y;
+                
+                if(X >= 5 || X <= -5)
+                {
+                    var heroMovePosition = transform.position + heroPosPattern[i];
+                    heroProp[i].unitNavMesh.SetDestination(heroMovePosition);
+                    heroProp[i].unitAnim.SetBool("isRun", true);
+                }
+                else if(Y >= 5 || Y <= -5)
+                {
+                    var heroMovePosition = transform.position + heroPosPattern[i];
+                    heroProp[i].unitNavMesh.SetDestination(heroMovePosition);
+                    heroProp[i].unitAnim.SetBool("isRun", true);
+                }
+                else
+                {
+                    heroProp[i].unitAnim.SetBool("isRun", false);
+                }
+            }
         }
+    }
+
+    public void GeneratePosPattern()
+    {
+        List<Vector3> tempPosPattern = new List<Vector3>();
+
+        switch (Heroes.Count)
+        {
+            case 1:
+                tempPosPattern.Add(new Vector3(0,0,0));
+                break;
+            case 2:
+                tempPosPattern.Add(new Vector3(-2f,0,0));
+                tempPosPattern.Add(new Vector3(2f,0,0));
+                break;
+            case 3:
+                tempPosPattern.Add(new Vector3(-2f,0,2f));
+                tempPosPattern.Add(new Vector3(2f,0,2f));
+                tempPosPattern.Add(new Vector3(0,0,-2f));
+                break;
+            case 4:
+                tempPosPattern.Add(new Vector3(0,0,2f));
+                tempPosPattern.Add(new Vector3(-2f,0,0));
+                tempPosPattern.Add(new Vector3(2f,0,0));
+                tempPosPattern.Add(new Vector3(0,0,-2f));
+                break;
+            case 5:
+                tempPosPattern.Add(new Vector3(-2f,0,2f));
+                tempPosPattern.Add(new Vector3(2f,0,2f));
+                tempPosPattern.Add(new Vector3(-2f,0,-2f));
+                tempPosPattern.Add(new Vector3(2f,0,-2f));
+                tempPosPattern.Add(new Vector3(0,0,0));
+                break;
+            default:
+                //персонажей нет
+                break;
+        }
+        heroPosPattern = tempPosPattern;
+    }
+
+    public void GetHeroProp()
+    {
+        List<UnitProp> tempHeroProp = new List<UnitProp>();
+        UnitProp unitProp = new UnitProp();
+
+        for(var i = 0; i < Heroes.Count; i++)
+        {
+            unitProp.unitAnim = Heroes[i].GetComponent<Animator>();
+            unitProp.unitTransform = Heroes[i].GetComponent<Transform>();
+            unitProp.unitNavMesh = Heroes[i].GetComponent<NavMeshAgent>();
+            tempHeroProp.Add(unitProp);
+        }
+        heroProp = tempHeroProp;
     }
 }
