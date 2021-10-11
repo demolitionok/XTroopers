8a47284b52c78c0e2269fffcb4c1840834bf3d08
Alexandr Olshynskyi <alexandrolshynskyi@gmail.com>
2021-10-12 01:12:08 +0300

    начал делать  мощный ии

---
+++ Assets/Scripts/EnemyAI/PatrollingState.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemyStates/PatrollingState")]
+public class PatrollingState : BaseState
+{
+    public override void Run()
+    {
+        
+    }
+}
