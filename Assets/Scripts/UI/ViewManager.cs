bed42609bbc269f58aaa5f50f885e2653e973f3a
demolitionok <51877153+demolitionok@users.noreply.github.com>
2021-10-15 02:15:14 +0200

    minor refactor

--- Assets/Scripts/UI/ViewManager.cs
+++ Assets/Scripts/UI/ViewManager.cs
@@ -2,70 +2,52 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class ViewManager : MonoBehaviour
 {
-    private static ViewManager instance;
+    private static ViewManager s_instance;
 
-    [SerializeField] private View[] _views;
-    [SerializeField] private View _startingView;
+    [SerializeField]
+    private View[] _views;
+    [SerializeField]
+    private View _startingView;
 
     private View _currentView;
     private readonly Stack<View> _history = new Stack<View>();
 
-    private void Awake() => instance = this;
+    private void Awake() => s_instance = this;
 
-    public T GetView<T>() where T : View
-    {
-        for (int i = 0; i < instance._views.Length; i++)
-        {
-            if (instance._views[i] is T view)
-            {
-                return view;
-            }
-        }
-
-        return null;
-    }
+    public T GetView<T>() where T : View => s_instance._views.FirstOrDefault(v => v is T) as T;
 
     public void ShowView<T>(bool remember = true) where T : View
     {
-        for (int i = 0; i < instance._views.Length; i++)
-        {
-            if (instance._views[i] is T view)
-            {
-                if (instance._currentView != null)
-                {
-                    if (remember)
-                    {
-                        instance._history.Push(view);
-                    }
-                    instance._currentView.Hide();
-                }
-                instance._views[i].Show();
-                instance._currentView = instance._views[i];
-            }
-        }
+        var view = GetView<T>();
+        if (view != null)
+            ShowView(view);
+        else
+            Debug.Log("view was null");
     }
     public void ShowView(View view, bool remember = true)
     {
-        if (instance._currentView != null)
+        if (s_instance._currentView != null)
         {
             if (remember)
             {
-                instance._history.Push(view);
+                s_instance._history.Push(view);
             }
-            instance._currentView.Hide();
+            s_instance._currentView.Hide();
         }
         view.Show();
 
-        instance._currentView = view;
+        s_instance._currentView = view;
     }
 
     public void ShowLast()
     {
-        if(instance._history.Count != 0)
-            instance.ShowView(instance._history.Pop(), false);
+        if(s_instance._history.Count != 0)
+            s_instance.ShowView(s_instance._history.Pop(), false);
     }
 }
