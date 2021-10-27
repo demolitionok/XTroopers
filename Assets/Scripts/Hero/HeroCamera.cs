c14da6ec82ee1df6f6d511121c179587ce41bb89
wolf4ok <demon.roge@gmail.com>
2021-10-27 09:34:43 +0300

    новая фишки камеры, активируются у лидера

--- Assets/Scripts/Hero/HeroCamera.cs
+++ Assets/Scripts/Hero/HeroCamera.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class HeroCamera : MonoBehaviour
 {
-	public Transform target;
-	public CameraProp camStat;
-	public Vector3 offset;
-	public Vector3 correction;
-	public float zoomSpeed;
-	public float minZoom;
-	public float maxZoom;
-	public float curZoom;
-	public float pitch;
-	public float yawSpeed;
-	public float curYaw;
+	[SerializeField] public Transform target;
+	[SerializeField] public CameraProp camStat;
+	[SerializeField] public Vector3 offset;
+	[SerializeField] public Vector3 correction;
+	[SerializeField] public float minZoom;
+	[SerializeField] public float maxZoom;
+	[SerializeField] public float curZoom;
+	private float zoomSpeed;
+	private float yawSpeed;
+	[SerializeField] public float Yaw;
+	[SerializeField] public float followYaw;
+	[SerializeField] public float curYaw;
+	private float pitch;
+    private bool cameraFollow = false; //камера поворачивает за лидером
+    private bool cameraStab = false; //отцентрировать камеру при движении, если изменяли поворот
+	private bool cameraIsFollow = false;
+
 	void Start()
 	{
 		offset = camStat.offset;
@@ -25,15 +31,97 @@ public class HeroCamera : MonoBehaviour
 		curZoom = camStat.curZoom;
 		pitch = camStat.pitch;
 		yawSpeed = camStat.yawSpeed;
-		curYaw = camStat.curYaw;
+		Yaw = camStat.curYaw;
+		curYaw = Yaw;
+		followYaw = Yaw;
 	}
+
 	void Update()
 	{
+		CameraUpd();
+	}
+
+	private void CameraUpd()
+	{
+		CameraYaw();
 		curZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
 		curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
-		curYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
 		transform.position = (target.position + correction) - offset * curZoom;
-		transform.LookAt((target.transform.position + correction) + Vector3.up * pitch);
+		transform.LookAt((target.position + correction) + Vector3.up * pitch);
 		transform.RotateAround((target.position + correction), Vector3.up, curYaw);
+	}
+
+	private void CameraYaw()
+	{
+		curYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+
+		if(cameraFollow)
+		{
+			followYaw = target.localEulerAngles.y + Yaw;
+		}
+		if(cameraStab && cameraFollow)
+		{
+			float tempYaw = 0;
+			if(curYaw > followYaw)
+			{
+				tempYaw = 0.3f;
+				curYaw -= tempYaw * yawSpeed * Time.deltaTime;
+			}
+			if(curYaw < followYaw)
+			{
+				tempYaw = -0.3f;
+				curYaw -= tempYaw * yawSpeed * Time.deltaTime;
+			}
+			float differenceYaw = followYaw - curYaw;
+			if(differenceYaw > -0.4 && differenceYaw < 0.4)
+			{
+				cameraStab = false;
+				curYaw = followYaw;
+			}
+		}
+		if(cameraStab && !cameraFollow)
+		{
+			float tempYaw = 0;
+			if(curYaw > Yaw)
+			{
+				tempYaw = 0.3f;
+				curYaw -= tempYaw * yawSpeed * Time.deltaTime;
+			}
+			if(curYaw < Yaw)
+			{
+				tempYaw = -0.3f;
+				curYaw -= tempYaw * yawSpeed * Time.deltaTime;
+			}
+			float differenceYaw = Yaw - curYaw;
+			if(differenceYaw > -0.4 && differenceYaw < 0.4)
+			{
+				cameraStab = false;
+				curYaw = Yaw;
+			}
+		}
+	}
+
+	public void CameraStab()
+	{
+		cameraStab = true;
+	}
+
+	public void CameraFollow(bool followCam)
+	{
+		cameraFollow = followCam;
+		if(followCam)
+		{
+			cameraIsFollow = true;
+			cameraStab = true;
+		}
+		else
+		{
+			if(cameraIsFollow)
+			{
+				cameraIsFollow = false;
+				cameraStab = true;
+			}
+		}
+			
 	}
 }
